72f06f6f3c9921518ec3b09c74c3a1fab05d17a9
Harry Callahan <hcallahan@lowrisc.org>
2022-04-27 15:14:48 +0100

    WIP

--- nix/riscv_gcc_lowrisc.nix
+++ nix/riscv_gcc_lowrisc.nix
@@ -19,7 +19,7 @@ stdenv.mkDerivation rec {
     cp -r * $out
   '';
   preFixup = ''
-    find $out -type f | while read f; do
+    find $out -type f \( ! -iname ".o" \) | while read f; do
       patchelf "$f" > /dev/null 2>&1 || continue
       patchelf --set-interpreter $(cat ${stdenv.cc}/nix-support/dynamic-linker) "$f" || true
       patchelf --set-rpath ${lib.makeLibraryPath [ "$out" stdenv.cc.cc.lib ncurses5 python27 ]} "$f" || true
