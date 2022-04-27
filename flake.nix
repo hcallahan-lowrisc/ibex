01f6a0a4b1c0648bbedad87e98d82520bee3a47f
Harry Callahan <hcallahan@lowrisc.org>
2022-04-27 16:26:02 +0100

    Create nix develop shell from flake with toolchain

--- flake.nix
+++ flake.nix
@@ -3,14 +3,26 @@
 
   inputs = { };
 
-  outputs = { self, nixpkgs, }: 
+  outputs = { self, nixpkgs, }:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
       riscv_gcc_toolchain = pkgs.callPackage ./nix/riscv_gcc_lowrisc.nix {};
+      simple_system = pkgs.mkShell rec {
+        name = "simple_system";
+        version = "0.1.0";
+        src = ./.;
+        buildInputs = with pkgs; [ verilator ] ++
+                      [ riscv_gcc_toolchain ];
+
+        # dontBuild = true;
+        # dontInstall = true;
+      };
     in
       {
 
-        defaultPackage.x86_64-linux = riscv_gcc_toolchain;
+        # defaultPackage.x86_64-linux = simple_system;
+        devShell.x86_64-linux = simple_system;
 
       };
 }
--- nix/riscv_gcc_lowrisc.nix
+++ nix/riscv_gcc_lowrisc.nix
@@ -1,7 +1,10 @@
 { pkgs, lib, stdenv,
-  fetchzip, autoPatchelfHook,
+  fetchzip,
   zlib, ncurses5, python27,
 }:
+
+# Used for reference...
+# https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/compilers/gcc-arm-embedded/10/default.nix
 
 stdenv.mkDerivation rec {
   name = "lowrisc-riscv-gcc-toolchain";
@@ -10,21 +13,24 @@ stdenv.mkDerivation rec {
     url = "https://github.com/lowRISC/lowrisc-toolchains/releases/download/${version}/lowrisc-toolchain-gcc-rv32imc-${version}.tar.xz";
     sha256 = "1m708xfdzf3jzclm2zw51my3nryvlsfwqkgps3xxa0xnhq4ly1bl";
   };
-  # nativeBuildInputs = [
-  #   autoPatchelfHook
-  # ];
-  # buildInputs = with pkgs; [ libmpc mpfr gmp expat ];
+
+  dontConfigure = true;
+  dontBuild = true;
+  dontPatchELF = true; # We will do this manually
+  dontStrip = true;
+
   installPhase = ''
     mkdir -p $out
     cp -r * $out
   '';
   preFixup = ''
-    find $out -type f \( ! -iname ".o" \) | while read f; do
+    find $out -type f ! -name ".o" | while read f; do
       patchelf "$f" > /dev/null 2>&1 || continue
       patchelf --set-interpreter $(cat ${stdenv.cc}/nix-support/dynamic-linker) "$f" || true
-      patchelf --set-rpath ${lib.makeLibraryPath [ "$out" stdenv.cc.cc.lib ncurses5 python27 ]} "$f" || true
+      patchelf --set-rpath ${lib.makeLibraryPath [ "$out" stdenv.cc.cc ncurses5 python27 ]} "$f" || true
     done
   '';
+
   # postFixup =
   #   let
   #     libPath =
